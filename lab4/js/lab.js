6d3432dae22db3d92132512e4d869b05202a064c
sickofsadness <lavimidnight@gmail.com>
2025-04-21 17:48:50 -0700

    Lab 4

--- lab4/js/lab.js
+++ lab4/js/lab.js
@@ -1,6 +1,143 @@
-// index.js - purpose and description here
-// Author: Your Name
-// Date:
+
+/**
+* index.js - Lab 4, Intro to Pseudocode
+* Author: Lavinia Chen <lchen256@ucsc.edu>
+* Date: 4/21/2025
+* Task 1: Making New England Clam Chowder
+*Task 2: Making Rock Paper Scissors
+* Making a pseudocode list for each task
+*/
+
+
+//task 1: making new england clam chowder
+
+//get ingredients
+  //potatoes
+  //6 strips of sacon
+  //clams
+  //butter
+  //2 celery
+  //garlic
+  //1 yellow onion
+  //hot sauce
+  //worcestershire sauce
+  //flour
+  //chicken broth
+  //half & half
+  //chicken bouillon cube
+  //bay leaf
+  //black pepper
+  //fresh parsley
+
+//get seasonings
+  //dried oregano
+  //dries parsley
+  //dried thyme
+  //salt
+  //smoked paprika
+  //black pepper
+
+//Measure out Ingredients
+  //6 pieces of bacon
+  //2 tablespoons of butter
+  //3 cloves of garlic
+  //1 teaspoon hot sauce
+  //1/3 cup flour
+  //1 cup chicken broth
+  //3 cups half & half
+  //8oz. clam juice
+  //1 chicken bouillon cube
+  //1 1/4 lb potatoes
+  //1 bay leaf
+
+  //measure out seasonings
+    //1/2 teaspoon dried oregano
+    //1/2 teaspoon dried parsley
+    //1/4 teaspoon dried Tthyme
+    //1/4 teaspoon salt
+    //1/8 teaspoon smoked paprika
+    //1/8 teaspoon black pepper
+
+//chop up ingredients
+  //peel and dice potatoes
+  //dice onion
+  //mince garlic
+  //chop clams
+
+//follow steps in recipe to cook
+  //cook bacon
+    //low heat
+    //set aside bacon on towel
+    //save 2 tablespoons of drippings
+
+  //add bacon and butter
+    //medium heat
+    //add diced onions and celery
+    //soften for 5-6 minutes
+    //add garlic
+    //add worcestershire sauce
+    //add seasonings
+    //cook 1 minute
+  
+  //add flour
+    //cook 2 minutes
+    //stir continuously
+  
+  //add chicken broth
+    //add bay leaf
+    //add canned clam juice
+    //stir in additional clam juice
+
+  //bring soup to gentle boil
+    //reduce to simmer
+    //partically cover with lid
+    //simmer for 20 minutes
+
+  //add in potatoes
+    //let potatoes cook for 20-25 minutes
+
+  //stir in clams
+    //let clams heat for 5 minutes
+    //remove bay leaf
+
+//laddle and serve clam chowder
+  //garnish with chapped parsley
+  //garnish with black pepper
+  //garnish with chopped bacon
+
+
+
+//task 2: rock paper scissors
+
+//start game
+  //give options of rock, paper, and scissors
+  //let player decide
+  //player clicks one decision
+  //comupter generates a decision of rock, paper, or scissors
+
+//Show on screen the results
+  //player's decision
+  //computer decision
+
+//compare 
+  //if both are the same, say "draw"
+    //restart game after 5 seconds
+    //bring player back to decision screen
+    //computer generates a decision
+  //game rules
+    //rock < paper
+    //rock > scissors
+    //scissors > paper
+    //rock = rock
+    //paper = paper
+    //scissors = scissors
+
+//determine if player or computer has won
+
+//play again screen
+  //decision to "play again"
+  //
+
 
 // Constants
 
